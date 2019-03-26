1b9dcda3bd1edf020bce2ec57097a7e9227b6eaa
domobrada@gmail.com <student@lab-xxx.fesb.hr>
2019-03-26 16:22:18 +0100

    First commit

---
+++ domo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace domo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
